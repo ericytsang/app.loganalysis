882f68714e32a89886672cc77e5f67c90cf97268
Eric Tsang <surplus.et@gmail.com>
2025-05-13 21:13:10 -0700

    background of the window will follow the theme as well now

--- composeApp/src/desktopMain/kotlin/org/example/project/App.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -2,15 +2,21 @@ package org.example.project
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -21,35 +27,44 @@ import kotlinproject.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App()
 {
-    MaterialTheme(colors = darkColors())
+    MaterialTheme(colors = lightColors())
     {
         var showContent by remember { mutableStateOf(false) }
 
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
         )
         {
-            Button(
-                onClick = { showContent = !showContent },
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             )
             {
-                Text("Click me!")
-            }
-
-            AnimatedVisibility(showContent)
-            {
-                val greeting = remember { Greeting().greet() }
-
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                Button(
+                    onClick = { showContent = !showContent },
                 )
                 {
-                    Image(painterResource(Res.drawable.compose_multiplatform),null)
-                    Text("Compose: $greeting")
+                    Text("Click me!")
+                }
+
+                AnimatedVisibility(showContent)
+                {
+                    val greeting = remember { Greeting().greet() }
+
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    )
+                    {
+                        Image(painterResource(Res.drawable.compose_multiplatform),null)
+                        Text("Compose: $greeting")
+                    }
                 }
             }
         }
     }
 }
+
+
