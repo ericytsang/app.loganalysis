70885f49e6280163f8aa107dea0e1725bb59f4e0
Eric Tsang <surplus.et@gmail.com>
2025-05-13 21:23:20 -0700

    make it possible to toggle the theme of the app

--- composeApp/src/desktopMain/kotlin/org/example/project/App.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -4,11 +4,11 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.darkColors
@@ -16,55 +16,90 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
 
+enum class Theme
+{
+    LIGHT,
+    DARK,
+}
+
 @Composable
 @Preview
 fun App()
 {
-    MaterialTheme(colors = lightColors())
+    var theme by remember { mutableStateOf(Theme.LIGHT) }
+
+    val themeColors = when (theme)
+    {
+        Theme.LIGHT -> lightColors()
+        Theme.DARK -> darkColors()
+    }
+
+    fillMaxBackground(colors = themeColors)
     {
         var showContent by remember { mutableStateOf(false) }
 
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background)
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
         )
         {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
+            Button(
+                onClick = { showContent = !showContent },
             )
             {
-                Button(
-                    onClick = { showContent = !showContent },
+                Text("Click me!")
+            }
+
+            Button(
+                onClick =
+                    {
+                        theme = when (theme)
+                        {
+                            Theme.LIGHT -> Theme.DARK
+                            Theme.DARK -> Theme.LIGHT
+                        }
+                    },
+            )
+            {
+                Text("Toggle theme")
+            }
+
+            AnimatedVisibility(showContent)
+            {
+                val greeting = remember { Greeting().greet() }
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 )
                 {
-                    Text("Click me!")
-                }
-
-                AnimatedVisibility(showContent)
-                {
-                    val greeting = remember { Greeting().greet() }
-
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    )
-                    {
-                        Image(painterResource(Res.drawable.compose_multiplatform),null)
-                        Text("Compose: $greeting")
-                    }
+                    Image(painterResource(Res.drawable.compose_multiplatform),null)
+                    Text("Compose: $greeting")
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun fillMaxBackground(
+    colors:Colors,
+    content:@Composable ColumnScope.()->Unit,
+)
+{
+    MaterialTheme(colors = colors)
+    {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            content = content,
+        )
+    }
+}
