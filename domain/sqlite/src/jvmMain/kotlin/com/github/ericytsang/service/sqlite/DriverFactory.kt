7ba8034b682c133c371d3862554102784b03e101
Eric Tsang <surplus.et@gmail.com>
2025-05-17 11:45:58 -0700

    create and store the db file as sql.db in ~/.com.github.ericytsang.loganalyzer

--- domain/sqlite/src/jvmMain/kotlin/com/github/ericytsang/service/sqlite/DriverFactory.kt
+++ domain/sqlite/src/jvmMain/kotlin/com/github/ericytsang/service/sqlite/DriverFactory.kt
@@ -3,10 +3,25 @@ package com.github.ericytsang.service.sqlite
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import java.util.Properties
 import app.cash.sqldelight.db.SqlDriver
+import java.io.File
 
-actual class DriverFactory {
-  actual fun createDriver(): SqlDriver {
-    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:test.db", Properties(),SqlDelightDatabase.Schema)
-    return driver
-  }
+actual class DriverFactory
+{
+    actual fun createDriver(appPackageName:String):SqlDriver
+    {
+        // use the user home directory to store the database file
+        val userHomePath = System.getProperty("user.home")
+        val appHomePath = File(userHomePath,".$appPackageName")
+        val appDatabasePath = File(appHomePath,"sql.db")
+
+        // need to create the directory if it doesn't exist, otherwise the database file won't be created
+        appHomePath.mkdirs()
+
+        // create the database driver
+        return JdbcSqliteDriver(
+            url = "jdbc:sqlite:${appDatabasePath.toURI().toASCIIString()}",
+            properties = Properties(),
+            schema = SqlDelightDatabase.Schema,
+        )
+    }
 }
