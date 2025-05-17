395f257c386f6df5cefb7a99063045d9063bf23f
Eric Tsang <surplus.et@gmail.com>
2025-05-17 12:17:33 -0700

    add comments, refactor code based on best practices

--- domain/sqlite/src/jvmMain/kotlin/com/github/ericytsang/service/sqlite/DatabaseDriverFactory.kt
+++ domain/sqlite/src/jvmMain/kotlin/com/github/ericytsang/service/sqlite/DatabaseDriverFactory.kt
@@ -5,6 +5,11 @@ import java.util.Properties
 import app.cash.sqldelight.db.SqlDriver
 import java.io.File
 
+/**
+ * This is the implementation of the DatabaseDriverFactory for JVM.
+ * It creates a SQLite database driver using the JDBC driver.
+ * The database file is stored in the user's home directory.
+ */
 actual class DatabaseDriverFactory
 {
     actual fun createDriver(appPackageName:String):SqlDriver
@@ -13,11 +18,12 @@ actual class DatabaseDriverFactory
         val appHomePath = getAndCreateAppHome(appPackageName)
 
         // use the user home directory to store the database file
-        val appDatabasePath = File(appHomePath,"sql.db")
+        val appDatabasePath = File(appHomePath,DB_FILE_NAME)
+        val appDatabaseFileUri = appDatabasePath.toURI().toASCIIString()
 
         // create the database driver
         return JdbcSqliteDriver(
-            url = "jdbc:sqlite:${appDatabasePath.toURI().toASCIIString()}",
+            url = "$SQLITE_JDBC_CONNECTIONS_SCHEME$appDatabaseFileUri",
             properties = Properties(),
             schema = SqlDelightDatabase.Schema,
         )
@@ -35,5 +41,11 @@ actual class DatabaseDriverFactory
     {
         val userHomePath = System.getProperty("user.home")
         return File(userHomePath)
+    }
+
+    companion object
+    {
+        private const val DB_FILE_NAME = "app-data.sqldelight.db"
+        private const val SQLITE_JDBC_CONNECTIONS_SCHEME = "jdbc:sqlite:"
     }
 }
