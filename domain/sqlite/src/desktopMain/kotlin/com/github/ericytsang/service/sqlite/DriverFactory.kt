3906bcef0f4669f650fe725d5e62d92faec11630
Eric Tsang <surplus.et@gmail.com>
2025-05-17 11:08:10 -0700

    delete unused

--- domain/sqlite/src/desktopMain/kotlin/com/github/ericytsang/service/sqlite/DriverFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.github.ericytsang.service.sqlite
-
-import app.cash.sqldelight.db.SqlDriver
-
-actual class DriverFactory {
-  actual fun createDriver(): SqlDriver {
-    return NativeSqliteDriver(Database.Schema, "test.db")
-  }
-}
