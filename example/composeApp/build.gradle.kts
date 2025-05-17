8e90c509b89e113d24c2a2fc8384f3c0e2bc47a0
Eric Tsang <surplus.et@gmail.com>
2025-05-17 10:30:15 -0700

    Revert "delete things that are not related to desktop implementation from the exameple"
    This reverts commit 4982b4c81101ddadd7a89c343826a5649384aa0b.

--- example/composeApp/build.gradle.kts
+++ example/composeApp/build.gradle.kts
@@ -4,8 +4,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 
     //Room step2 -> plugins
     id("androidx.room") version "2.7.0-alpha04"
@@ -13,8 +14,26 @@ plugins {
 }
 
 kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    
     jvm("desktop")
     
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
     // Room step6 part1 for adding ksp src directory to use AppDatabase::class.instantiateImpl() in iosMain:
     // Due to https://issuetracker.google.com/u/0/issues/342905180
     sourceSets.commonMain {
@@ -24,6 +43,10 @@ kotlin {
     sourceSets {
         val desktopMain by getting
         
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+        }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -49,6 +72,43 @@ kotlin {
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.8.1") //for v
 
         }
+    }
+}
+
+android {
+    namespace = "com.sunildhiman90.cmpwithroom"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        applicationId = "com.sunildhiman90.cmpwithroom"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+    dependencies {
+        debugImplementation(compose.uiTooling)
     }
 }
 
