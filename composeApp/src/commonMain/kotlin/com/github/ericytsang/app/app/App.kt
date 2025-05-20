6db06c68a98be086a4fb48368c1bd8c5ff4277ed
Eric Tsang <surplus.et@gmail.com>
2025-05-20 00:11:42 -0700

    moved the new composable to its own file

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/app/App.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
-import androidx.compose.material.Colors
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -17,10 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.withStyle
 import com.github.ericytsang.app.model.Dimens
 import com.github.ericytsang.app.ui.modal.openWorkingFileSetEditorInNewWindowBlocking
 import com.github.ericytsang.app.util.animatedThemeColors
@@ -30,7 +25,6 @@ import com.github.ericytsang.domain.objects.WorkingFileSetEmpty
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import java.awt.Window
 import java.io.File
-import kotlin.random.Random
 
 @Composable
 @Preview
@@ -128,102 +122,3 @@ fun App(
         }
     }
 }
-
-@Composable
-fun ColorCodedLogLine(
-    text:String,
-    themeForColorCoding:Theme,
-    modifier: Modifier = Modifier,
-)
-{
-    val logLineString = buildColorCodedLogLine(
-        logLine = text,
-        isDarkTheme = when (themeForColorCoding)
-        {
-            Theme.LIGHT -> false
-            Theme.DARK -> true
-        },
-    )
-    Text(
-        text = logLineString,
-        modifier = modifier,
-    )
-}
-
-
-/**
- * Function to get a color appropriate for the theme for a word based on its hash code.
- * @param word The word to be colored
- * @param isDarkTheme Boolean indicating if the theme is dark
- * @return Color for the word
- */
-fun getColorForWord(word:String,isDarkTheme:Boolean):Color
-{
-    // generate a random color based on the hash code of the word
-    val random = Random(word.hashCode())
-    val red = random.nextFloat()*0.5f
-    val green = random.nextFloat()*0.5f
-    val blue = random.nextFloat()*0.5f
-
-    // adjust brightness based on theme
-    val colorBoost = if (isDarkTheme) 0.5f else 0f
-
-    // create the Color object
-    return Color(
-        red = red+colorBoost,
-        green = green+colorBoost,
-        blue = blue+colorBoost,
-    )
-}
-
-/**
- * Function to build a color-coded log line
- * @param logLine The log line to be color-coded
- * @param isDarkTheme Boolean indicating if the theme is dark
- * @return AnnotatedString with color-coded words
- */
-fun buildColorCodedLogLine(
-    logLine:String,
-    isDarkTheme:Boolean,
-):AnnotatedString
-{
-    val delimiters = ":,\"<>(){}[]. ".toSet()
-    val builder = AnnotatedString.Builder()
-
-    var currentWord = StringBuilder()
-    for (char in logLine)
-    {
-        if (char in delimiters)
-        {
-            // Add the current word with its color
-            if (currentWord.isNotEmpty())
-            {
-                val word = currentWord.toString()
-                builder.withStyle(SpanStyle(color = getColorForWord(word,isDarkTheme)))
-                {
-                    append(word)
-                }
-                currentWord.clear()
-            }
-            // Add the delimiter as is
-            builder.append(char)
-        }
-        else
-        {
-            currentWord.append(char)
-        }
-    }
-    // Add the last word if any
-    if (currentWord.isNotEmpty())
-    {
-        val word = currentWord.toString()
-        builder.withStyle(SpanStyle(color = getColorForWord(word,isDarkTheme)))
-        {
-            append(word)
-        }
-    }
-
-    return builder.toAnnotatedString()
-}
-
-
---
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/app/ColorCodedLogLine.kt
@@ -0,0 +1,109 @@
+package com.github.ericytsang.app.app
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.withStyle
+import com.github.ericytsang.domain.objects.Theme
+import kotlin.random.Random
+import kotlin.text.iterator
+
+@Composable
+fun ColorCodedLogLine(
+    text:String,
+    themeForColorCoding:Theme,
+    modifier:Modifier = Modifier.Companion,
+)
+{
+    val logLineString = buildColorCodedLogLine(
+        logLine = text,
+        isDarkTheme = when (themeForColorCoding)
+        {
+            Theme.LIGHT -> false
+            Theme.DARK -> true
+        },
+    )
+    Text(
+        text = logLineString,
+        modifier = modifier,
+    )
+}
+
+
+/**
+ * Function to get a color appropriate for the theme for a word based on its hash code.
+ * @param word The word to be colored
+ * @param isDarkTheme Boolean indicating if the theme is dark
+ * @return Color for the word
+ */
+private fun getColorForWord(word:String,isDarkTheme:Boolean):Color
+{
+    // generate a random color based on the hash code of the word
+    val random = Random(word.hashCode())
+    val red = random.nextFloat()*0.5f
+    val green = random.nextFloat()*0.5f
+    val blue = random.nextFloat()*0.5f
+
+    // adjust brightness based on theme
+    val colorBoost = if (isDarkTheme) 0.5f else 0f
+
+    // create the Color object
+    return Color(
+        red = red+colorBoost,
+        green = green+colorBoost,
+        blue = blue+colorBoost,
+    )
+}
+
+/**
+ * Function to build a color-coded log line
+ * @param logLine The log line to be color-coded
+ * @param isDarkTheme Boolean indicating if the theme is dark
+ * @return AnnotatedString with color-coded words
+ */
+private fun buildColorCodedLogLine(
+    logLine:String,
+    isDarkTheme:Boolean,
+):AnnotatedString
+{
+    val delimiters = ":,\"<>(){}[]. ".toSet()
+    val builder = AnnotatedString.Builder()
+
+    var currentWord = StringBuilder()
+    for (char in logLine)
+    {
+        if (char in delimiters)
+        {
+            // Add the current word with its color
+            if (currentWord.isNotEmpty())
+            {
+                val word = currentWord.toString()
+                builder.withStyle(SpanStyle(color = getColorForWord(word,isDarkTheme)))
+                {
+                    append(word)
+                }
+                currentWord.clear()
+            }
+            // Add the delimiter as is
+            builder.append(char)
+        }
+        else
+        {
+            currentWord.append(char)
+        }
+    }
+    // Add the last word if any
+    if (currentWord.isNotEmpty())
+    {
+        val word = currentWord.toString()
+        builder.withStyle(SpanStyle(color = getColorForWord(word,isDarkTheme)))
+        {
+            append(word)
+        }
+    }
+
+    return builder.toAnnotatedString()
+}
