7157be355eabd5c2060b1f2f5be572ca84720313
Eric Tsang <surplus.et@gmail.com>
2025-05-22 23:44:46 -0700

    delete unused

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/app/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.github.ericytsang.app.app
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/app/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ericytsang.app.app
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
