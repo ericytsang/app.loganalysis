00a50cd453d9f24af49445f5aca4ddc1188af0e7
Eric Tsang <surplus.et@gmail.com>
2025-05-25 23:13:48 -0700

    imports

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/app/App.kt
@@ -12,10 +12,8 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.github.ericytsang.app.model.Dimens
