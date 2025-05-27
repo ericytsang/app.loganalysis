4d706f7555b22bb50796b172d7bdc79ad2945d10
Eric Tsang <surplus.et@gmail.com>
2025-05-26 23:32:11 -0700

    imports

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/projectbrowser/ProjectBrowserViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/projectbrowser/ProjectBrowserViewModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
 
 /**
  * ViewModel for the Project Browser window.
