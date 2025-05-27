c0276be49b5b6136367c6d0a81ec2db854ba8859
Eric Tsang <surplus.et@gmail.com>
2025-05-27 00:04:29 -0700

    try to acquire system-wide lock file

--- composeApp/src/jvmMain/kotlin/org/example/project/main.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/main.kt
@@ -3,13 +3,18 @@ package org.example.project
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.github.ericytsang.app.ui.frame.app.App
+import com.github.ericytsang.app.util.EnsureSingletonProcessInstance
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Log Analyzer",
-    )
-    {
-        App(window = window)
+fun main()
+{
+    EnsureSingletonProcessInstance().tryLock()
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Log Analyzer",
+        )
+        {
+            App(window = window)
+        }
     }
 }
