c91cc493dd4dea847c6971849ff3cf207cb75cf4
Eric Tsang <surplus.et@gmail.com>
2025-05-27 01:24:15 -0700

    i just do a double check to ensure that we still have the file-based lock...

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/util/FileLocker.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/util/FileLocker.kt
@@ -2,6 +2,7 @@ package com.github.ericytsang.app.util
 
 import com.github.ericytsang.domain.appinfo.AppInfoService
 import java.io.File
+import java.time.Instant
 import javax.swing.JOptionPane
 import kotlin.system.exitProcess
 
@@ -16,7 +17,7 @@ class EnsureSingletonProcessInstance(
         val systemWideAppLockFile = File(appHome, "singleton-process.lock")
         if (!fileLocker.tryLock(systemWideAppLockFile))
         {
-            showFatalErrorDialog("App is already running.")
+            showFatalErrorDialog("App is already running. If it is not, please delete the lock file and try again. Lock file location:\n\n${systemWideAppLockFile.absolutePath}")
         }
     }
 
@@ -36,16 +37,16 @@ class FileLocker()
     fun tryLock(file:File):Boolean
     {
         // see if the file is already locked by another live process
-        if (isFileLockedByAnotherLiveProcess(file)) return false
+        if (isThereAnOngoingProcess(file)) return false
 
         // try to lock the file
         createIfNotExists(file)
-        val myPid = ProcessHandle.current().pid()
-        file.writeText(myPid.toString())
+        val myProcessInfo = getMyProcessInfo()
+        file.writeText("${myProcessInfo.pid}\n${myProcessInfo.startTime}")
 
         // double-check that the file is now locked by this process in case of race conditions
         Thread.sleep(500)
-        return !isFileLockedByAnotherLiveProcess(file)
+        return isTheLockFileStillLockedByUs(file)
     }
 
     private fun createIfNotExists(file:File)
@@ -57,22 +58,39 @@ class FileLocker()
         }
     }
 
-    private fun isFileLockedByAnotherLiveProcess(file:File):Boolean
+    private fun isThereAnOngoingProcess(file:File):Boolean
     {
-        // if file does not exist, then it cannot be locked
-        if (!file.exists()) return false
+        return getProcessInfoFromLockFile(file).fold(
 
-        // see if the file is already locked by another process
-        val lockingPid = file.reader().useLines { lines -> lines.firstOrNull()?.toLong() }
-        return lockingPid != null && isProcessAlive(lockingPid)
+            // check if the process info in the lock file matches any currently living process
+            onSuccess = { it != null && isProcessAlive(it) },
+
+            // assume that there is an ongoing process if the lock file is malformed
+            onFailure = { true },
+        )
     }
 
-    private fun isProcessAlive(processId:Long):Boolean
+    private fun isTheLockFileStillLockedByUs(file:File):Boolean
+    {
+        return getProcessInfoFromLockFile(file).fold(
+
+            // check if the process info in the lock file matches this process
+            onSuccess = { it != null && it == getMyProcessInfo() },
+
+            // assume that the lock file is not locked by us if it is malformed
+            onFailure = { false },
+        )
+    }
+
+    private fun isProcessAlive(processInfo:ProcessInfo):Boolean
     {
         return try
         {
+
             // Attempt to open the process with the given ID
-            ProcessHandle.of(processId).map { it.isAlive }.orElse(false)
+            ProcessHandle.of(processInfo.pid)
+                .map { it.isAlive && it.toProcessInfo() == processInfo }
+                .orElse(false)
         }
         catch (_:Exception)
         {
@@ -80,5 +98,37 @@ class FileLocker()
             false
         }
     }
-}
 
+    private fun getMyProcessInfo():ProcessInfo = ProcessHandle.current().toProcessInfo()
+
+    private fun getProcessInfoFromLockFile(file:File):Result<ProcessInfo?>
+    {
+        return runCatching()
+        {
+            // there is no lock file, then there is no process info
+            if (!file.exists()) return@runCatching null
+
+            // read the lock file and parse the process info
+            file.reader().useLines { lines ->
+                val lineList = lines.take(2).toList().takeIf { it.size == 2 } ?: error("Lock file is malformed")
+                ProcessInfo(
+                    pid = lineList[0].toLong(),
+                    startTime = Instant.parse(lineList[1]),
+                )
+            }
+        }
+    }
+
+    private fun ProcessHandle.toProcessInfo():ProcessInfo
+    {
+        return ProcessInfo(
+            pid = pid(),
+            startTime = info().startInstant().orElse(Instant.ofEpochSecond(0)),
+        )
+    }
+
+    private data class ProcessInfo(
+        val pid:Long,
+        val startTime:Instant
+    )
+}
