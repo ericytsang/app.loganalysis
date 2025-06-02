1eb5549083c7f20a77246f0a8ae12e1b4fcd41d2
Eric Tsang <surplus.et@gmail.com>
2025-06-02 01:06:00 -0700

    Revert "added a bunch of outlines around everything"
    This reverts commit e992d965f442c6035cf0948aae44bff8f98eafb4.

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/util/component/CommonWindowHeader.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/util/component/CommonWindowHeader.kt
@@ -5,11 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Colors
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,30 +22,23 @@ fun CommonWindowHeader(
     content: @Composable (() -> Unit) = {}
 )
 {
-    Surface(
+    Row(
         modifier = Modifier.fillMaxWidth(),
-        shape = MaterialTheme.shapes.small,
-        border = ButtonDefaults.outlinedBorder,
-    )
-    {
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(Dimens.mttPadding),
-            horizontalArrangement = Arrangement.spacedBy(Dimens.mttPadding),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+        horizontalArrangement = Arrangement.spacedBy(Dimens.mttPadding),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
 
-            // custom content
-            content()
+        // custom content
+        content()
 
-            // move the icon button the end
-            Spacer(modifier = Modifier.weight(1f,fill = true))
+        // move the icon button the end
+        Spacer(modifier = Modifier.weight(1f,fill = true))
 
-            // settings button
-            IconButton(
-                modifier = Modifier.padding(end = Dimens.mttPadding),
-                onClick = { childWindowManager.openSettingsDialog() },
-                content = { IconSettings(themeColors.onSurface) },
-            )
-        }
+        // settings button
+        IconButton(
+            modifier = Modifier.padding(end = Dimens.mttPadding),
+            onClick = { childWindowManager.openSettingsDialog() },
+            content = { IconSettings(themeColors.onSurface) },
+        )
     }
 }
