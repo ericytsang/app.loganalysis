1d9c043739cb6fbf96de79ea2b10a963ee3f3e31
Eric Tsang <surplus.et@gmail.com>
2025-06-02 01:44:29 -0700

    added an exit button to the app

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/util/component/CommonWindowHeader.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/util/component/CommonWindowHeader.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Colors
 import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,6 +17,7 @@ import com.github.ericytsang.app.model.Dimens
 import com.github.ericytsang.app.ui.util.asset.IconSettings
 import com.github.ericytsang.app.ui.util.ChildWindowManager
 import com.github.ericytsang.app.ui.util.openSettingsDialog
+import kotlin.system.exitProcess
 
 @Composable
 fun CommonWindowHeader(
@@ -30,6 +34,13 @@ fun CommonWindowHeader(
 
         // custom content
         content()
+
+        // exit button
+        Button(
+            onClick = { exitProcess(0) },
+            content = { Text("Exit app") },
+            colors = ButtonDefaults.buttonColors(themeColors.error),
+        )
 
         // move the icon button the end
         Spacer(modifier = Modifier.weight(1f,fill = true))
