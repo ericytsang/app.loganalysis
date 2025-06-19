479ac5d4ff19d763aa1e6c121cebfc626c19d9cd
Eric Tsang <surplus.et@gmail.com>
2025-06-18 20:36:51 -0700

    deleted unused code

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/workingfileseteditor/collapsableEditWorkingFilesSection.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/workingfileseteditor/collapsableEditWorkingFilesSection.kt
@@ -3,7 +3,6 @@ package com.github.ericytsang.app.ui.frame.workingfileseteditor
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
