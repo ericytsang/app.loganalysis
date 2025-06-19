6163fe4cd19ec3bf4961bf7b138535b770af5420
Eric Tsang <surplus.et@gmail.com>
2025-06-19 08:11:27 -0700

    added a model for tracking the selected items

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/LogViewerRoot.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/LogViewerRoot.kt
@@ -55,6 +55,57 @@ import com.github.ericytsang.domain.objects.WorkingFileSetEmpty
 import kotlinx.coroutines.CoroutineScope
 import sh.calvin.reorderable.rememberReorderableLazyListState
 
+/**
+ * sealed class representing a set of selected items.
+ * there are 2 implementations:
+ * - one representing "selected all items except for the ones in a set of excluded items"
+ * - another representing "selected no items except for the ones in a set of included items"
+ */
+sealed class SelectedItems<T>
+{
+    fun isSelected(item:T):Boolean = when (this)
+    {
+        is IncludeSelected -> item in includedItems
+        is ExcludeSelected -> item !in excludedItems
+    }
+
+    fun add(item:T):SelectedItems<T> = when (this)
+    {
+        is IncludeSelected -> IncludeSelected(includedItems + item)
+        is ExcludeSelected -> ExcludeSelected(excludedItems - item)
+    }
+
+    fun remove(item:T):SelectedItems<T> = when (this)
+    {
+        is IncludeSelected -> IncludeSelected(includedItems - item)
+        is ExcludeSelected -> ExcludeSelected(excludedItems + item)
+    }
+
+    fun toggle(item:T):SelectedItems<T> = when (isSelected(item))
+    {
+        true -> remove(item)
+        false -> add(item)
+    }
+
+    /**
+     * selected no items except for the ones in a set of included items.
+     */
+    data class IncludeSelected<T>(val includedItems:Set<T>):SelectedItems<T>()
+
+    /**
+     * selected all items except for the ones in a set of excluded items.
+     */
+    data class ExcludeSelected<T>(val excludedItems:Set<T>):SelectedItems<T>()
+}
+
+// clicking on a log line will select the clicked log line, while also deselecting all other log lines.
+
+// click+dragging on a log line will select all log lines between the last clicked log line and the clicked log line, while also deselecting all other log lines.
+
+// ctrl+clicking on a log line will toggle the selection of the clicked log line, while also keeping the other already-selected log lines selected.
+
+// shift+clicking on a log line will select all log lines between the last clicked log line and the clicked log line, while also keeping the other already-selected log lines selected.
+
 @Composable
 fun LogViewerRoot(
     themeColors:Colors,
