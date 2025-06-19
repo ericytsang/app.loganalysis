24cc6fbcfb4dfb5f46cf8df022571cf9ee100c0e
Eric Tsang <surplus.et@gmail.com>
2025-06-18 20:37:39 -0700

    moved ReorderableSidebarItemKey to its own file

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/FilterSetViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/FilterSetViewModel.kt
@@ -1,8 +1,6 @@
 package com.github.ericytsang.app.ui.frame.logviewer
 
 import com.github.ericytsang.domain.objects.ConfigurationId
-import com.github.ericytsang.domain.objects.FilePath
-import com.github.ericytsang.domain.objects.FilterId
 import com.github.ericytsang.domain.objects.FilterInterpretationMode
 import com.github.ericytsang.domain.objects.FilterType
 import com.github.ericytsang.domain.repo.dependencyinjection.RepositoryDependencyProvider
@@ -82,20 +80,3 @@ class FilterTypeFilterSetViewModelFactory(
     }
 }
 
-sealed interface ReorderableSidebarItemKey
-{
-    /** key for non reorderable item */
-    data class Other(val key:String):ReorderableSidebarItemKey
-
-    /**
-     * key for the "new filter" button which will be treated as the user
-     * trying to add the filter to the top of the section.
-     */
-    data class NewFilterButton(val filterType:FilterType):ReorderableSidebarItemKey
-
-    /** key for reorderable filter item */
-    data class FilterItem(val filterId:FilterId):ReorderableSidebarItemKey
-
-    /** key for reorderable log file item */
-    data class LogFileItem(val filePath:FilePath):ReorderableSidebarItemKey
-}
