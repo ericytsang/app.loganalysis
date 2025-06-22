731c6e8b959a32a43e54c318e0602a08e89bdfb3
Eric Tsang <surplus.et@gmail.com>
2025-06-22 14:01:09 -0700

    done: - update model for tracking selection state so that there is a new subclass: UnionSelected
    todo:
    - expand selection up/down with keyboard shortcuts
    - moving focus up/down with keyboard keys
    - click+dragging on a log line will select all log lines between the last clicked log line and the clicked log line, while also deselecting all other log lines.

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/SelectedItems.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/SelectedItems.kt
@@ -1,46 +1,53 @@
 package com.github.ericytsang.app.ui.frame.logviewer
 
-/**
- * sealed class representing a set of selected items.
- * there are 2 implementations:
- * - one representing "selected all items except for the ones in a set of excluded items"
- * - another representing "selected no items except for the ones in a set of included items"
- */
-sealed class SelectedItems<T>
+interface SelectedItems<T>
 {
-    operator fun contains(item:T):Boolean = isSelected(item)
+    fun isSelected(item:T):Boolean
+    fun add(item:T):SelectedItems<T>
+    fun remove(item:T):SelectedItems<T>
+}
 
-    fun isSelected(item:T):Boolean = when (this)
-    {
-        is IncludeSelected -> item in includedItems
-        is ExcludeSelected -> item !in excludedItems
-    }
+operator fun <T> SelectedItems<T>.contains(item:T):Boolean = isSelected(item)
 
-    fun add(item:T):SelectedItems<T> = when (this)
-    {
-        is IncludeSelected -> IncludeSelected(includedItems + item)
-        is ExcludeSelected -> ExcludeSelected(excludedItems - item)
-    }
+operator fun <T> SelectedItems<T>.plus(item:T):SelectedItems<T> = add(item)
 
-    fun remove(item:T):SelectedItems<T> = when (this)
-    {
-        is IncludeSelected -> IncludeSelected(includedItems - item)
-        is ExcludeSelected -> ExcludeSelected(excludedItems + item)
-    }
+operator fun <T> SelectedItems<T>.minus(item:T):SelectedItems<T> = remove(item)
 
-    fun toggle(item:T):SelectedItems<T> = when (item in this)
-    {
-        true -> remove(item)
-        false -> add(item)
-    }
+fun  <T> SelectedItems<T>.toggle(item:T):SelectedItems<T> = when (item in this)
+{
+    true -> remove(item)
+    false -> add(item)
+}
 
-    /**
-     * selected no items except for the ones in a set of included items.
-     */
-    data class IncludeSelected<T>(val includedItems:Set<T> = emptySet()):SelectedItems<T>()
+/**
+ * selected no items except for the ones in a set of included items.
+ */
+data class IncludeSelected<T>(val includedItems:Set<T> = emptySet()):SelectedItems<T>
+{
+    override fun isSelected(item:T):Boolean = item in includedItems
+    override fun add(item:T):SelectedItems<T> = IncludeSelected(includedItems + item)
+    override fun remove(item:T):SelectedItems<T> = IncludeSelected(includedItems - item)
+}
 
-    /**
-     * selected all items except for the ones in a set of excluded items.
-     */
-    data class ExcludeSelected<T>(val excludedItems:Set<T> = emptySet()):SelectedItems<T>()
+/**
+ * selected all items except for the ones in a set of excluded items.
+ */
+data class ExcludeSelected<T>(val excludedItems:Set<T> = emptySet()):SelectedItems<T>
+{
+    override fun isSelected(item:T):Boolean = item !in excludedItems
+    override fun add(item:T):SelectedItems<T> = ExcludeSelected(excludedItems-item)
+    override fun remove(item:T):SelectedItems<T> = ExcludeSelected(excludedItems+item)
+}
+
+/**
+ * selected all items in a union of two sets of selected items.
+ */
+data class UnionSelected<T>(
+    val a:SelectedItems<T>,
+    val b:SelectedItems<T>,
+):SelectedItems<T>
+{
+    override fun isSelected(item:T):Boolean = item in a || item in b
+    override fun add(item:T):SelectedItems<T> = UnionSelected(a + item, b)
+    override fun remove(item:T):SelectedItems<T> = UnionSelected(a - item, b - item)
 }
