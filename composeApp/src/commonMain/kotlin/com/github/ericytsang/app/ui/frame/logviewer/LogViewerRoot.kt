27256cdbf9e7fdbceda21e72d1f71f21512b1544
Eric Tsang <surplus.et@gmail.com>
2025-06-22 20:04:43 -0700

    imports

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/LogViewerRoot.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/frame/logviewer/LogViewerRoot.kt
@@ -47,7 +47,6 @@ import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.github.ericytsang.app.model.Dimens
-import com.github.ericytsang.app.ui.frame.newprojectwizard.SelectedFile
 import com.github.ericytsang.app.ui.util.ChildWindowManager
 import com.github.ericytsang.app.ui.util.asset.IconEditFileList
 import com.github.ericytsang.app.ui.util.asset.IconExcludeFilter
