138a744322bae85bcfae61a5b59024eab829e94a
Eric Tsang <surplus.et@gmail.com>
2025-06-22 21:15:43 -0700

    added horizontal scrolling

--- composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/util/component/LazyColumnWithScrollbar.kt
+++ composeApp/src/commonMain/kotlin/com/github/ericytsang/app/ui/util/component/LazyColumnWithScrollbar.kt
@@ -1,11 +1,15 @@
 package com.github.ericytsang.app.ui.util.component
 
+import androidx.compose.foundation.HorizontalScrollbar
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +19,7 @@ import androidx.compose.ui.Modifier
 fun LazyColumnWithScrollbar(
     modifier:Modifier = Modifier,
     lazyListState:LazyListState = rememberLazyListState(),
+    horizontalScrollState:ScrollState = rememberScrollState(),
     content:LazyListScope.()->Unit,
 )
 {
@@ -23,12 +28,19 @@ fun LazyColumnWithScrollbar(
     )
     {
         LazyColumn(
-            state = lazyListState,content = content,
+            modifier = Modifier.horizontalScroll(horizontalScrollState),
+            state = lazyListState,
+            content = content,
         )
 
         VerticalScrollbar(
             modifier = Modifier.align(Alignment.CenterEnd),
             adapter = rememberScrollbarAdapter(lazyListState)
+        )
+
+        HorizontalScrollbar(
+            modifier = Modifier.align(Alignment.BottomStart),
+            adapter = rememberScrollbarAdapter(horizontalScrollState)
         )
     }
 }
